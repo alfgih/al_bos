bb4cca0bf37549a0f1d64175861646e9caa8c4e2
alfgih <alfgihebrahim@gmail.com>
2020-04-16 23:08:04 +0300

    Django 3.0.5 installed

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Alfgih\Desktop\python\Myproject\Blogger\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
